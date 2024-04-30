3e4d39efa0098a12dee776427a004e8bbb1a42c7
Ignacio Joaquin Riolffi <iriolff@frba.utn.edu.ar>
2024-04-30 16:15:34 -0300

    Subo anotaciones de clase SSL

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+void imprimirTablaGrados() {
+    int fahr;
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
+        printf("%3d %6.1lf\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    }
+}
+
+void copia_de_archivos() {
+    int c;
+    c = getchar();
+    while (c != EOF) {
+        putchar(c);
+        c = getchar();
+    }
+}
+
+void conteo_de_caracteres() {
+    long nc;
+    nc = 0;
+    while (getchar() != EOF) {
+        nc++;
+        printf("%ld\n", nc);
+    }
+}
+
+void conteo_de_lineas() {
+    int c, nl;
+    nl = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') {
+            ++nl;
+        }
+    }
+    printf("%d\n", nl);
+}
+
+int main() {
+    //imprimirTablaGrados();
+    //copia_de_archivos();
+    //conteo_de_caracteres();
+    //conteo_de_lineas();
+    //puts("Puta"); imprimir una cadena pero sin formato, mas simple y agrega el \n solo
+    //cout y cin son variables que representan un flujo
+    //fprintf es una version mas generica del printf solo que este ultimo imprime en consola y fprintf nos permite elegir
+    //en que flujo imprimir
+
+
+}
